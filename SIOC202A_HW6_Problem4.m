720740229d6ccb219741c0bc2c34eb857fafcecc
Flow619 <88358248+Flow619@users.noreply.github.com>
2024-02-26 14:24:57 -0800

    Update SIOC202A_HW6_Problem4.m
    - Add Code Comments

--- SIOC202A_HW6_Problem4.m
+++ SIOC202A_HW6_Problem4.m
@@ -1,37 +1,38 @@
 %% SIOC 202A
 %% Homework #6
 %% Problem #4
-
 % Author: Trenton Saunders
 % Date: 02-24-2024
 
+% Description: This code approximaes the ray trace for accoustic wave with
+% a linearly increasing sound speed profile.
 %%
 close all;
 clear all;
 clc;
 
 %% User Defined Variables
-dz = 5;    % depth step
+dz = 5;    % depth domain grid step (size of layer of constant sound speed)
 z_bounds = [0,500];
 
-launch_angle = 5; %deg
-z_source = 250;
+launch_angle = 5;    % deg
+z_source = 250;    % source depth [m]
 x_source = 0;
 
-t_end = 10; 
+t_end = 10;    % end time of numerical approximation
 
 %% Set up Variables
 z = z_bounds(1):dz:z_bounds(2); % depth array
 
 % Sound Speed Profile
-c.z = z_bounds(1) + (dz/2):dz: z_bounds(2) - (dz/2); % Average z values (between z array values)
-c.profile = 1500 + 0.1.*c.z; % sound speed profile
+c.z = z_bounds(1) + (dz/2):dz: z_bounds(2) - (dz/2); % Average z values for layer (between z array values)
+c.profile = 1500 + 0.1.*c.z; % sound speed profile for layer (average value)
 
 dt = (1/2) * dz/max(c.profile); % want to make sure time step small enough to not skip between sound speed layers!
 
 t = 0:dt:t_end;   % Time Series.
 
-%% Calculation
+%% Ray Trace - Time loop
 x_old = x_source;
 z_old = z_source;
 
@@ -62,7 +63,7 @@ for i = 1:length(t)
 
     if c_new ~= c_old   % If moved between layers!
 
-        theta_new = acosd( (c_new/c_old)*cosd(theta_old) );  % Snell's law, for moving between layers.
+        theta_new = acosd( (c_new/c_old)*cosd(theta_old) );  % Snell's law, moving between sound speed layers.
 
         if ~isreal(theta_new)  % if imag. # (assume at bottom boundary)
             theta_new = -theta_old; % flip the angle (@ boundary).
